0ef0d0b3c6c91a0624e70a83a977f930669be75a
Gustavo <gustavoalfonso05@hotmail.com>
2020-05-23 00:53:52 -0500

    Consulta de totales

--- modules/app/controllers/devoluciones.py
+++ modules/app/controllers/devoluciones.py
@@ -4,6 +4,7 @@ from flask_jwt_extended import jwt_required
 from app import app, mongo, jwt
 from app.core.upload import upload_file
 from app.core.read import exec_process
+from bson.son import SON
 import logger
 
 ROOT_PATH = os.environ.get('ROOT_PATH')
@@ -17,10 +18,61 @@ def unauthorized_response(callback):
 @jwt_required
 def devolucion():
     if request.method == 'GET':
-        data = mongo.db.devoluciones.find({})
+        tipo = request.args.get('tipo')
+        if tipo == 'poranio':
+            # Suma total de cada año
+            pipeline = [
+                {'$group': {'_id': '$anio', 'total': {'$sum': '$bandejas'}}},
+                {'$sort': SON([('_id', 1)])}
+            ]
+        elif tipo == 'pormes':
+            # Suma total de cada mes de un año
+            try:
+                anio = int(request.args.get('anio'))
+            except:
+                return {'ok': False, 'message': 'El año no es válido'}, 400
+            if anio == None or anio == '':
+                return {'ok': False, 'message': 'Debe ingresar el año en el parámetro -anio-'}, 400
+            pipeline = [
+                {'$match': {'anio': anio}},
+                {'$group': {'_id': '$mes', 'total': {'$sum': '$bandejas'}}},
+                {'$sort': SON([('_id', 1)])}
+            ]
+        elif tipo == 'pordia':
+            # Suma total de cada día de un mes de un año
+            try:
+                anio = int(request.args.get('anio'))
+                mes = int(request.args.get('mes'))
+            except:
+                return {'ok': False, 'message': 'El año o el mes no es válido'}, 400
+            if anio == None or anio == '' or mes == None or mes == '':
+                return {'ok': False, 'message': 'Debe ingresar el año en el parámetro -anio- y el mes en el parámetro -mes-'}, 400
+            pipeline = [
+                {'$match': {'anio': anio, 'mes': mes}},
+                {'$group': {'_id': '$dia', 'total': {'$sum': '$bandejas'}}},
+                {'$sort': SON([('_id', 1)])}
+            ]
+        elif tipo == 'porlocal':
+            # Suma total de cada local de un día de un mes de un año
+            try:
+                anio = int(request.args.get('anio'))
+                mes = int(request.args.get('mes'))
+                dia = int(request.args.get('dia'))
+            except:
+                return {'ok': False, 'message': 'El año, el mes o el día no es válido'}, 400
+            if anio == None or anio == '' or mes == None or mes == '' or dia == None or dia == '':
+                return {'ok': False, 'message': 'Debe ingresar el año en el parámetro -anio-m el mes en el parámetro -mes- y el día en el parámetro -dia-'}, 400
+            pipeline = [
+                {'$match': {'anio': anio, 'mes': mes, 'dia': dia}},
+                {'$group': {'_id': '$local', 'total': {'$sum': '$bandejas'}}},
+                {'$sort': SON([('total', -1)])}
+            ]
+        else:
+            return {'ok': False, 'message': 'Debe ingresar una opción válida en el parámetro -tipo-'}, 400
+        data = mongo.db.devoluciones.aggregate(pipeline)
         devoluciones = []
         for devolucion in data:
-            devoluciones.append({'ciudad': devolucion['ciudad'], 'local': devolucion['local'], 'anio': devolucion['anio'], 'mes': devolucion['mes'], 'dia': devolucion['dia'], 'bandejas': devolucion['bandejas']})
+            devoluciones.append({tipo.replace('por', ''): devolucion['_id'], 'total': devolucion['total']})
         return {'ok': True, 'data': devoluciones}, 200
 
 @app.route('/upload', methods=['POST'])
