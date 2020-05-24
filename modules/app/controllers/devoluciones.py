b67de6c35cf0ce3bd0739cf0aaeb0b7a4d86e5c7
Gustavo <gustavoalfonso05@hotmail.com>
2020-05-24 15:44:42 -0500

    consulta de un local en el mes

--- modules/app/controllers/devoluciones.py
+++ modules/app/controllers/devoluciones.py
@@ -61,11 +61,40 @@ def devolucion():
             except:
                 return {'ok': False, 'message': 'El año, el mes o el día no es válido'}, 400
             if anio == None or anio == '' or mes == None or mes == '' or dia == None or dia == '':
-                return {'ok': False, 'message': 'Debe ingresar el año en el parámetro -anio-m el mes en el parámetro -mes- y el día en el parámetro -dia-'}, 400
+                return {'ok': False, 'message': 'Debe ingresar el año en el parámetro -anio- el mes en el parámetro -mes- y el día en el parámetro -dia-'}, 400
             pipeline = [
                 {'$match': {'anio': anio, 'mes': mes, 'dia': dia}},
                 {'$group': {'_id': '$local', 'dato': {'$sum': '$bandejas'}}},
                 {'$sort': SON([('dato', -1)])}
+            ]
+        elif tipo == 'porlocalenelmes':
+            # Suma total agrupada por local de un mes de un año
+            try:
+                anio = int(request.args.get('anio'))
+                mes = int(request.args.get('mes'))
+            except:
+                return {'ok': False, 'message': 'El año o el mes no es válido'}, 400
+            if anio == None or anio == '' or mes == None or mes == '':
+                return {'ok': False, 'message': 'Debe ingresar el año en el parámetro -anio- y el mes en el parámetro -mes-'}, 400
+            pipeline = [
+                {'$match': {'anio': anio, 'mes': mes}},
+                {'$group': {'_id': '$local', 'dato': {'$sum': '$bandejas'}}},
+                {'$sort': SON([('dato', -1)])}
+            ]
+        elif tipo == 'deunlocalenelmes':
+            # Suma total agrupada por dia de un local en un mes de un año
+            try:
+                local = request.args.get('local')
+                mes = int(request.args.get('mes'))
+                anio = int(request.args.get('anio'))
+            except:
+                return {'ok': False, 'message': 'El local, el mes o el año no es válido'}, 400
+            if local == None or local == '' or mes == None or mes == '' or anio == None or anio == '':
+                return {'ok': False, 'message': 'Debe ingresar el año en el parámetro -anio-, el mes en el parámetro -mes- y el local en el parametro -local-'}, 400
+            pipeline = [
+                {'$match': {'local': local, 'anio': anio, 'mes': mes}},
+                {'$group': {'_id': '$dia', 'dato': {'$sum': '$bandejas'}}},
+                {'$sort': SON([('_id', 1)])}
             ]
         else:
             return {'ok': False, 'message': 'Debe ingresar una opción válida en el parámetro -tipo-'}, 400
